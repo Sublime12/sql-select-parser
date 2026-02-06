a3642d4350de7f6031e3980e5330ee6c3d0aae3f
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-02-06 15:05:35 -0500

    add struct for sql query program

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,96 @@
 const std = @import("std");
-const sql_select_parser = @import("sql_select_parser");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try sql_select_parser.bufferedPrint();
+    const query =
+        \\\ select c1, c2
+        \\\ from  table
+        \\\ where condition
+    ;
+    _ = query;
+
+    const from = FromClause.init("table");
+
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var columns = SelectClause.Columns.empty;
+    defer columns.deinit(allocator);
+
+    try columns.append(allocator, "c1");
+    try columns.append(allocator, "c2");
+
+    const select = SelectClause.init(columns);
+
+    const expr = Expr.init(select, from, null);
+
+    std.debug.print("expr: {f}", .{expr});
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+const Expr = struct {
+    columns: SelectClause,
+    table: FromClause,
+    where: ?FilterClause,
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    pub fn init(
+        columns: SelectClause,
+        table: FromClause,
+        where: ?FilterClause,
+    ) Expr {
+        return .{
+            .columns = columns,
+            .table = table,
+            .where = where,
+        };
+    }
+
+    pub fn format(
+        self: Expr,
+        writer: *std.io.Writer,
+    ) !void {
+        try writer.print("SELECT {f} FROM {f}", .{ self.columns, self.table });
+
+        if (self.where) |w| {
+            try writer.print(" WHERE {any}", .{w});
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
+        try writer.print("\n", .{});
+    }
+};
+
+const SelectClause = struct {
+    const Columns = std.ArrayList([]const u8);
+    columns: Columns,
+
+    pub fn init(columns: Columns) SelectClause {
+        return .{
+            .columns = columns,
+        };
+    }
+
+    pub fn format(
+        self: SelectClause,
+        writer: *std.io.Writer,
+    ) !void {
+        for (self.columns.items) |col| {
+            try writer.print("{s}, ", .{col});
+        }
+    }
+};
+
+const FromClause = struct {
+    name: []const u8,
+    pub fn init(name: []const u8) FromClause {
+        return .{ .name = name };
+    }
+
+    pub fn format(
+        self: FromClause,
+        writer: *std.io.Writer,
+    ) !void {
+        try writer.print("{s}", .{self.name});
+    }
+};
+
+const FilterClause = union(enum) {
+    a: i32,
+};
