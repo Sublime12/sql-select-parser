236364d2ecf9ad497d7f733ee5de0ff335ba69d5
Sublime Tshimpangila <tshimangasublime@gmail.com>
2026-02-18 20:01:09 -0500

    add test for select where eql = id

--- src/main.zig
+++ src/main.zig
@@ -81,3 +81,37 @@ test "run simple select from table" {
     try execute(allocator, &result, &table, &expr);
     try std.testing.expect(result.rows.items.len == 20);
 }
+
+test "run simple select from table with where = id" {
+    const query =
+        \\ select col2, col1, from table1 where col1 = 6 
+    ;
+
+    var buffer: [1024]u8 = undefined;
+    var errwriter = std.fs.File.stderr().writer(&buffer);
+    const stderr = &errwriter.interface;
+
+    const allocator = std.testing.allocator;
+    var lexer = Lexer.init(allocator, query, query.len, "select.sql");
+    defer lexer.deinit();
+
+    var parser = Parser.init(allocator, &lexer);
+    var expr = try parser.parse();
+    defer expr.deinit(allocator);
+
+    var table = try createTestTable(allocator);
+    defer table.deinit(allocator);
+
+    var result: Table = .empty;
+    defer result.deinit(allocator);
+
+    try execute(allocator, &result, &table, &expr);
+    try result.print(stderr);
+    try std.testing.expect(result.rows.items.len == 1);
+    const row = result.rows.items[0];
+    try std.testing.expect(row.items.len == 2);
+    try std.testing.expect(row.items[0] == 7);
+    try std.testing.expect(row.items[1] == 6);
+
+    try stderr.flush();
+}
